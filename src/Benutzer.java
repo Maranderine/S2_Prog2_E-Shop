0d9b645c99e174e8e100f5e3b045e5e6eb66a983
Jonah Wichmann <jonahake.wichmann@gmail.com>
2022-05-08 14:54:15 +0200

    Soweit ich weiß alles außer der CUI

--- src/Benutzer.java
+++ src/Benutzer.java
@@ -1,2 +1,68 @@
+import java.util.Date;
+
 public class Benutzer {
+
+    //Klassenvariablen
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    public Benutzer(String username, String password, String firstname, String lastname, Date registerDate, Date lastLoginDate) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return Gibt den Benutzernamen aus.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return Gibt den Vornamen aus.
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @return Gibt den Nachnamen aus.
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @return Erstellt den Vornamen.
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return Erstellt den Nachnamen.
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return Erstellt Passwort, wenn es nicht dem alten entspricht.
+     */
+    public boolean setPassword(String oldPassword, String newPassword) {
+        if(oldPassword.compareTo(this.password) == 0) {
+            this.password = newPassword;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
---
+++ src/DataObjects/Rechnung.java
@@ -0,0 +1,33 @@
+package DataObjects;
+
+import java.util.Date;
+
+public class Rechnung {
+
+    //Klassenvariablen
+    private String rechnungsId;
+    private Date kaufDatum;
+    // private inhalt;
+
+    //Konstruktor für Rechnung
+    // public Rechnung(Kunde kunde, Date kaufDatum, inhalt) {
+    //        this.rechnungsId = "0-" + kunde.getKundenNr() + "-" + (100 + kunde.getKundenRechnungen().size());   //info ????
+    //        this.kaufDatum = kaufDatum;
+    //        this.inhalt = inhalt;
+    //    }
+
+    //Getter für das Kaufdatum, welcher das Kaufdatum übergibt.
+    public Date getKaufDatum() {
+        return kaufDatum;
+    }
+
+    //Getter für die Rechnungs ID der Rechnung, der die Rechnungs ID übergibt.
+    public String getRechnungsId() {
+        return rechnungsId;
+    }
+
+    //Wenn abgefragt wird der Wert auf 0 und addiert den Preis des Artikels dazu(z.B. Artikel & Anzahl)
+    public void getGesamtPreis() {
+        double betrag = 0;
+    }
+}
