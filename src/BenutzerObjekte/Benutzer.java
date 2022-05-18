93ec779df876d6f56c23041a1f154b2d0f5d857e
Maranderine <104585818+Maranderine@users.noreply.github.com>
2022-05-18 15:13:37 +0200

    Update Benutzer.java

--- src/BenutzerObjekte/Benutzer.java
+++ src/BenutzerObjekte/Benutzer.java
@@ -1,20 +1,16 @@
 package BenutzerObjekte;
 
-import java.util.Date;
-
-public class Benutzer {
+abstract class Benutzer {
 
     //Klassenvariablen
     private String username;
     private String password;
-    private String firstname;
-    private String lastname;
+    private String name;
 
-    public Benutzer(String username, String password, String firstname, String lastname, Date registerDate, Date lastLoginDate) {
+    public Benutzer(String name, String username, String password) {
+        this.name = name;
         this.username = username;
         this.password = password;
-        this.firstname = firstname;
-        this.lastname = lastname;
     }
 
     public String getUsername() {
@@ -25,20 +21,12 @@ public class Benutzer {
         return password;
     }
 
-    public String getFirstname() {
-        return firstname;
+    public String getName() {
+        return name;
     }
 
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public boolean setPassword(String oldPassword, String newPassword) {
