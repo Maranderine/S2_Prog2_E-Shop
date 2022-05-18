c7e63e3aff7dc282fb3de9aaaca77efb8fb7318f
Maranderine <104585818+Maranderine@users.noreply.github.com>
2022-05-18 15:32:37 +0200

    Update Mitarbeiter.java

--- src/BenutzerObjekte/Mitarbeiter.java
+++ src/BenutzerObjekte/Mitarbeiter.java
@@ -1,60 +1,15 @@
 package BenutzerObjekte;
 
-public class Mitarbeiter {
-    private String name;
-    private String email;
-    private String strasse = "";
+public class Mitarbeiter extends Benutzer{
     private int mitarbeiterNr;
-    private String plz = "";
-    private String wohnort = "";
 
-    public Mitarbeiter(int nr, String name) {
+    public Mitarbeiter(String name, String username, String password, int nr) {
+        super(name, username, password);
         mitarbeiterNr = nr;
-        this.name = name;
     }
 
     // Methoden zum Setzen und Lesen der Kunden-Eigenschaften,
     // z.B. getStrasse() und setStrasse()
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPlz() {
-        return plz;
-    }
-
-    public void setPlz(String plz) {
-        this.plz = plz;
-    }
-
-    public String getStrasse() {
-        return strasse;
-    }
-
-    public void setStrasse(String strasse) {
-        this.strasse = strasse;
-    }
-
-    public String getWohnort() {
-        return wohnort;
-    }
-
-    public void setWohnort(String wohnort) {
-        this.wohnort = wohnort;
-    }
 
     public int getKundenNr() {
         return mitarbeiterNr;
